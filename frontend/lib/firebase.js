d9b4c96f6efdab8fe95c1feb1db85523ed6c3bb3
ishagolakiya6101 <ishagolakiya1@gmail.com>
2025-12-10 17:13:41 +0530

    frontend changed

---
+++ frontend/lib/firebase.js
@@ -0,0 +1,13 @@
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: process.env.PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.PUBLIC_FIREBASE_PROJECT_ID,
+};
+
+const app = !getApps().length ? initializeApp(firebaseConfig) : getApps()[0];
+export const auth = getAuth(app);
+export const db = getFirestore(app);
